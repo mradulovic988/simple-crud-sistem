a3c2b5f1c0951f61f2fa5242597434b537dc8eb5
Marko <upss070288@gmail.com>
2020-09-01 13:56:02 +0200

    Fixed belogsToMany() relation

--- app/Article.php
+++ app/Article.php
@@ -16,10 +16,15 @@ class Article extends Model
     public function path()
     {
         return route('articles.show', $this);
-    }
+    } 
 
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
