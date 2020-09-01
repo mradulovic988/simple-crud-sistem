cd76ea100abc30c8416421782d417f0391095bfa
Marko <upss070288@gmail.com>
2020-09-01 17:52:37 +0200

    Fixed errors in show.index and created tags

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -6,15 +6,17 @@
 	<div id="page" class="container">
 		<div id="content">
 			<div class="title">
-                @foreach ($articles as $article)
+                @forelse ($articles as $article)
                     {{-- We can do on this way below --}}
                     {{-- <a href="{{ route('articles.show', $article) }}"> --}}
-                    <a href="{{ $article->path() }}"></a>
-                        <h2>{{ $article->title }}</h2>
+                    <a href="{{ $article->path() }}"><h2>{{ $article->title }}</h2></a>
+                        
                         <p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
-                        {{ $article->body }}
+                        {{ $article->excerpt }}
                     </a>
-                @endforeach
+                    @empty
+                        <p>No relevant articles yet.</p>
+                @endforelse
 		</div>
 		
 	</div>
